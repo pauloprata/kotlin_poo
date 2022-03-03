5b21b7c696475ad9caf044d13301c0211056b5b1
Paulo Prata <paulodanielprata@gmail.com>
2022-03-03 10:00:59 -0300

    created class gerente

--- Modules/src/Gerente.kt
+++ Modules/src/Gerente.kt
@@ -1,2 +1,8 @@
-class Gerente {
+class Gerente(
+        nome: String,
+        cpf: String,
+        salario: Double)
+    : Funcionario(nome = nome, cpf = cpf,salario= salario) {
+    override fun calcularAuxilio(): Double = salario * 0.4
+
 }
