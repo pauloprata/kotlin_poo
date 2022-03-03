b4601dce979555f5ed1026d9265e81bebea8b7c6
Paulo Prata <paulodanielprata@gmail.com>
2022-03-03 10:47:37 -0300

    created class cliente

---
+++ Modules/src/Cliente.kt
@@ -0,0 +1,15 @@
+class Cliente(
+        nome: String,
+        cpf: String,
+        val clienteTipo: ClienteTipo,
+        val senha: String
+): Pessoa(nome, cpf),Login {
+    override fun login(): Boolean = "123456" == senha
+
+
+    override  fun toString(): String = """
+                Nome: $nome
+                Cpf: $cpf
+                TipoCliente: ${clienteTipo.descricao}
+        """.trimIndent()
+}
